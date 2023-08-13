3dbc5d27976aee0848f79b2693223b701508206e
David Huang <108887691+excaeruleo@users.noreply.github.com>
2023-08-13 19:56:00 -0400

    Delete forms.py

--- forms.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
-
-class TaskForm(FlaskForm):
-	label = StringField('label', validators = [DataRequired()])
